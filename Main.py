668daa18e5c2e083a2ae1b178f0c670c40fb6a41
Kubster96 <jungiewiczjakub@gmail.com>
2018-01-04 18:13:13 +0100

    lowpass filter

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pyaudio
 import time
 import numpy as np
+from copy import deepcopy
 from butter_transform import butter_bandpass_filter, transform
 from matplotlib import pyplot as plt
 import scipy.signal as signal
@@ -8,60 +9,66 @@ import scipy.signal as signal
 CHANNELS = 1
 RATE = 44100
 
+r=0
+
 p = pyaudio.PyAudio()
 fulldata = np.array([])
 dry_data = np.array([])
 counter = 0
 DROP_FIRST = 3
-buffer = []
+first = 44100
+index = 0
+
+samples = []
+delay_time = 200
+number_of_repeats = 3
+samples_size = 0
 
 
 def main():
+    global samples_size
+
+    samples_size = int((delay_time/1000) * number_of_repeats * RATE // 1024)
+
     stream = p.open(format=pyaudio.paFloat32,
                     channels=CHANNELS,
                     rate=RATE,
                     output=True,
                     input=True,
-                    stream_callback=callback)
+                    stream_callback=delay_callback)
 
     stream.start_stream()
 
     while stream.is_active():
-        time.sleep(10)
+        time.sleep(20)
         stream.stop_stream()
     stream.close()
 
     p.terminate()
 
 
-def callback2(in_data, frame_count, time_info, flag):
-    global b, a, fulldata  # global variables for filter coefficients and array
+def delay_callback(in_data, frame_count, time_info, flag):
+    global samples, index
+
+    n = int((delay_time/1000)*RATE//1024)
+    # n to jest ile elementow ma jeden przedzial
+
     audio_data = np.fromstring(in_data, dtype=np.float32)
-    # do whatever with data, in my case I want to hear my data filtered in realtime
-    # audio_data = signal.filtfilt(b, a, audio_data, padlen=200).astype(np.float32).tostring()
-    fulldata = np.append(fulldata, audio_data)
-    # saves filtered data in an array
-    return audio_data, pyaudio.paContinue
+    copy = deepcopy(audio_data)
 
+    while index > samples_size:
 
-def callback(in_data, frame_count, time_info, flag):
-    global b, a, fulldata, dry_data, counter
-    audio_data = np.fromstring(in_data, dtype=np.float32)
+        print(len(samples))
+        print(n)
+        print()
 
-    if counter > 100:
-        f, Pxx_spec = signal.welch(audio_data,RATE, 'flattop', 1024, scaling='spectrum')
-        plt.figure()
-        plt.semilogy(f, np.sqrt(Pxx_spec))
-        plt.xlabel('frequency [Hz]')
-        plt.ylabel('Linear spectrum [V RMS]')
-        plt.show()
+        for j in range(0, number_of_repeats):
+            audio_data += samples[j*n]
+        audio_data = audio_data/(number_of_repeats + 1)
+        samples.pop(0)
 
-        plt.plot(np.linspace(0,1,1024),audio_data)
-        plt.show()
-
-
-
-    counter += 1
+    index = index + 1
+    samples.append(copy)
 
     return audio_data, pyaudio.paContinue
 
