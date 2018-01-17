cb2ece68d569d6d3a55134b947bf071ab81338ee
RobertootreboR <hrodeberaht@gmail.com>
2018-01-17 21:39:42 +0100

    user can decide how fast the volume drops

--- delay/delay3.py
+++ delay/delay3.py
@@ -11,31 +11,28 @@ p = pyaudio.PyAudio()
 
 samples = []
 
-max_delay =2500
+max_delay = 2500
 max_repeats = 5
 max_volume = 5
+max_volume_drop=5
 
-gui = myGui.init(max_delay,max_repeats,max_volume,1)
-
+gui = myGui.init(max_delay, max_repeats, max_volume, max_volume_drop)
 
 max_sample_size = int((max_delay / 1000) * max_repeats * RATE / 1024)
-
-
 
 delay = 500
 number_of_repeats = 4
 samples_size = int((delay / 1000) * number_of_repeats * RATE / 1024)
-single_sample_size = samples_size // number_of_repeats # do ustawienia
+single_sample_size = samples_size // number_of_repeats
 index = 0
 
 
 def update():
-    global delay,number_of_repeats,samples_size,single_sample_size
+    global delay, number_of_repeats, samples_size, single_sample_size
     delay = gui['delay'].get()
-    number_of_repeats=gui['repeats'].get()
+    number_of_repeats = gui['repeats'].get()
     samples_size = int((delay / 1000) * number_of_repeats * RATE / 1024)
     single_sample_size = samples_size // number_of_repeats
-
 
 
 def main():
@@ -61,8 +58,8 @@ def main():
 def delay_callback(in_data, frame_count, time_info, flag):
     global samples, index
     update()
-    audio_data = np.fromstring(in_data, dtype=np.float32) *5 * gui['volume'].get()
-    if delay ==0:
+    audio_data = np.fromstring(in_data, dtype=np.float32) * 5 * gui['volume'].get()
+    if delay == 0:
         samples.append(audio_data)
         return audio_data, pyaudio.paContinue
     copy = deepcopy(audio_data)
@@ -70,14 +67,14 @@ def delay_callback(in_data, frame_count, time_info, flag):
     j = 0
 
     while i > 0 and j < number_of_repeats:
-        audio_data += samples[i]/(2**(j))
+        audio_data += samples[i] / (gui['volume_drop'].get() * j + 1)
         i = i - single_sample_size
         j = j + 1
-    audio_data = audio_data/(j + 1)
-    
+    audio_data = audio_data / (j + 1)
+
     if index > max_sample_size:
         samples.pop(0)
-        
+
     index = index + 1
     samples.append(copy)
 
--- gui/gui.py
+++ gui/gui.py
@@ -1,46 +1,49 @@
 from tkinter import *
 
 
-
-def init(max_delay, max_repeats,max_volume,factor,
+def init(max_delay, max_repeats, max_volume, max_drop,
          default_delay=700,
          default_repeats=4,
-         default_volume=1):
+         default_volume=1,
+         default_drop =2):
     master = Tk()
     master.title("Echo")
 
-    scaleDict ={}
+    scale_dict = {}
 
     delay = Scale(master, from_=0, to=max_delay,
-              resolution=100,
-              label="delay in ms",
-              orient=HORIZONTAL,
-              length=200)
+                  resolution=100,
+                  label="delay in ms",
+                  orient=HORIZONTAL,
+                  length=200)
     delay.grid(row=0, column=0, sticky=W)
     delay.set(default_delay)
-    scaleDict['delay']=delay
+    scale_dict['delay'] = delay
 
     repeats = Scale(master, from_=1, to=max_repeats,
-              label="number of repeats",
-              orient=HORIZONTAL,
-              length=200)
+                    label="number of repeats",
+                    orient=HORIZONTAL,
+                    length=200)
     repeats.grid(row=1, column=0, sticky=W)
     repeats.set(default_repeats)
-    scaleDict['repeats']=repeats
+    scale_dict['repeats'] = repeats
 
     volume = Scale(master, from_=0, to=max_volume,
-              resolution=0.1,
-              label="volume",
-              orient=HORIZONTAL,
-              length=200)
+                   resolution=0.1,
+                   label="volume",
+                   orient=HORIZONTAL,
+                   length=200)
     volume.grid(row=2, column=0, sticky=W)
     volume.set(default_volume)
-    scaleDict['volume']=volume
-    d = Scale(master, from_=20, to=-20, orient=HORIZONTAL)
-    d.grid(row=3, column=0, sticky=W)
-    d.set(factor)
-    #eqList.append(d)
-    return scaleDict
+    scale_dict['volume'] = volume
+    volume_drop = Scale(master, from_=0, to=max_drop,
+                        label='volume drop speed',
+                        orient=HORIZONTAL,
+                        length=200)
+    volume_drop.grid(row=3, column=0, sticky=W)
+    volume_drop.set(default_drop)
+    scale_dict['volume_drop'] = volume_drop
+    return scale_dict
 
 
 def start():
